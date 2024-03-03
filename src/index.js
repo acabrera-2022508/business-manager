4e5bc76567623177d7902313bcdae06d447ad27e
acabrera-2022508 <acabrera-2022508@kinal.edu.gt>
2024-03-03 15:07:17 -0600

    helpers and middlewares

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import connection from './database/mongo.js';
+import User from './models/user.model.js';
 import 'dotenv/config';
 
 const app = express();
@@ -17,8 +18,23 @@ app.use(cors());
 // Routes
 
 // Start server
-connection().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server on port ${process.env.PORT}`);
+connection()
+  .then(async () => {
+    const users = await User.find({});
+
+    if (users.length === 0) {
+      const user = new User({
+        name: 'admin',
+        lastName: 'admin',
+        username: 'admin',
+        password: 'admin',
+      });
+
+      await user.save();
+    }
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server on port ${process.env.PORT}`);
+    });
   });
-});
---
+++ src/middlewares/isLoggedIn.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import User from '../models/user.model.js';
+
+const isLoggedIn = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const { uid } = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(uid);
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: 'Invalid token or expired' });
+  }
+};
+
+export default isLoggedIn;
